b0b68e46beff2b8cf738a9fd70222a1df77c61e4
aleilias <86530708+aleilias@users.noreply.github.com>
2021-12-26 11:00:49 -0600

    jsx styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const img = "https://picsum.photos/200";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      My Favourite Foods
+    </h1>
+    <div>
+      <img src={img + "?grayscale"} alt="random" />
+      <img
+        className="food-img"
+        src="https://fusedbyfionauyema.com/wp-content/uploads/2021/02/Fused-by-Fiona-Uyema-Sushi-Q-A-how-to-make-sushi-at-home.jpg"
+        alt="Sushi"
+      />
+      <img
+        className="food-img"
+        src="https://static.toiimg.com/thumb/53110049.cms?width=1200&height=900"
+        alt="pizza"
+      />
+      <img
+        className="food-img"
+        src="https://iamafoodblog.b-cdn.net/wp-content/uploads/2020/05/homemade-birria-tacos-recipe-3273w-500x500.jpg"
+        alt="Tacos de birria"
+      />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
